5c924987ef2d223b5d98a90865d89733cc7ffc3f
NOKIMIMO <janikowski.nicolas@gmail.com>
2025-03-06 16:10:23 +0100

    fix?

---
+++ src/assets/values/imgPath.ts
@@ -0,0 +1,36 @@
+export const IMAGES_PATH: { [key: string]: string } = {
+  KT: "/svg/bacon.svg",
+  RUBY: "/svg/champignon.svg",
+  PEARL: "/svg/crevette.svg",
+  REACT: "/svg/crevette.svg",
+  KOBOL: "/svg/poulet.svg",
+  C: "/svg/onion.svg",
+  JAVA: "/svg/saumon.svg",
+  "C++": "/svg/saumon.svg",
+  "C#": "/svg/tomate.svg",
+  OTHER: "/svg/bacon.svg"
+};
+
+export enum INGREDIENT_TO_COLOR {
+    KT= "#FF0000",
+    RUBY= "#FFA500",
+    PEARL= "#FFFF00", 
+    KOBOL= "#008000",
+    C= "#0000FF",
+    JAVA= "#4B0082",
+    REACT= "#EE82EE",
+    SQL= "#FFA500",
+    "C++"= "#4B0082",
+    "C#"= "#EE82EE",
+    OTHER= "purple"
+}
+export const color_to_path: { [key: string]: string } = {
+    "#FF0000": "/svg/bacon.svg",
+    "#FFA500": "/svg/champignon.svg",
+    "#FFFF00": "/svg/crevette.svg",
+    "#008000": "/svg/poulet.svg",
+    "#0000FF": "/svg/onion.svg",
+    "#4B0082": "/svg/saumon.svg",
+    "#EE82EE": "/svg/tomate.svg",
+    "purple": "/svg/bacon.svg"
+};
